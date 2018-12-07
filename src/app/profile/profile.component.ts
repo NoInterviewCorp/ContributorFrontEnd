e7ac8203e844bc5986acb6eddfef8013eac81a80
Soham Sahare <sohamsahare@gmail.com>
2018-12-07 17:47:53 +0530

    User entity is posted to knowledge graph on profile page if doesn;t exist

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,43 +2,54 @@ import { Component, OnInit } from '@angular/core';
 import { CommunicatorService } from '../services/communicator.service';
 import { LearningPlan } from 'src/models/learningplan.model';
 import { Subscription } from 'rxjs';
+import { UserWrapper } from 'src/models/userprofile.model';
 
 @Component({
- selector: 'app-profile',
- templateUrl: './profile.component.html',
- styleUrls: ['./profile.component.css']
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: UserWrapper;
+  res: any;
 
- contributions: LearningPlan[];
- subscriptions: LearningPlan[];
- constructor(private communicator: CommunicatorService) {
- }
+  contributions: LearningPlan[];
+  subscriptions: LearningPlan[];
+  constructor(private communicator: CommunicatorService) {
+  }
 
- ngOnInit() {
-   this.getContributions();
-   this.getSubscriptions();
- }
+  ngOnInit() {
+    this.getContributions();
+    this.getSubscriptions();
+    this.postUserId();
+  }
 
- getContributions() {
-   this.communicator.getContributions().subscribe((contributions: LearningPlan[]) => {
-     this.contributions = contributions;
-   });
- }
+  postUserId() {
+    this.communicator.postUser(this.user).subscribe((res: UserWrapper) => {
+      this.user = res;
+      console.log(this.user);
+    })
+  }
 
- getSubscriptions() {
-   this.communicator.getSubscriptions().subscribe((subscriptions: LearningPlan[]) => {
-     this.subscriptions = subscriptions;
-   })
- }
+  getContributions() {
+    this.communicator.getContributions().subscribe((contributions: LearningPlan[]) => {
+      this.contributions = contributions;
+    });
+  }
 
- //handle Unsubscribe Func
- handleUnsubscribe(something) {
-   console.log(something);
-   const index = this.subscriptions.findIndex(
-     subscriptions => subscriptions.learningPlanId === something);
-     this.subscriptions.splice(index, 1);
-   console.log(this.subscriptions);
- }
+  getSubscriptions() {
+    this.communicator.getSubscriptions().subscribe((subscriptions: LearningPlan[]) => {
+      this.subscriptions = subscriptions;
+    })
+  }
+
+  //handle Unsubscribe Func
+  handleUnsubscribe(something) {
+    console.log(something);
+    const index = this.subscriptions.findIndex(
+      subscriptions => subscriptions.learningPlanId === something);
+    this.subscriptions.splice(index, 1);
+    console.log(this.subscriptions);
+  }
 
 }
